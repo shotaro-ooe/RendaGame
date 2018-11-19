8b41d2dfc252cc7ae109ddc3c95f3e69a228a705
shotaro-ooe <shoooe97@gmail.com>
2018-11-19 15:41:47 +0900

    八割完成

---
+++ Assets/EndSceneMager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndSceneMager : MonoBehaviour {
+    public Text lastScoreText;
+    public Text highScoreText;
+    int highScore;
+    int lastScore;
+
+	// Use this for initialization
+	void Start () {
+        lastScore = PlayerPrefs.GetInt("score");
+        lastScoreText.text = lastScore.ToString();
+
+        if (PlayerPrefs.HasKey("highScore") == true){
+            highScore = PlayerPrefs.GetInt("highScore");
+            if (highScore < lastScore){
+                highScore = lastScore;
+                PlayerPrefs.GetInt("highScore", highScore);
+            }
+        }else{
+            highScore = lastScore;
+            PlayerPrefs.SetInt("highScore", highScore);
+
+        }
+        highScoreText.text = highScore.ToString();
+
+		
+	}
+    public void RetryButton(){
+        SceneManager.LoadScene("StartScene");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class GameManagerScript : MonoBehaviour {
+
+    int count;
+    public Text countText;
+    public Text timerText;
+    float timer = 10f;
+    public Text buttonText;
+
+    bool isPlaying = false;
+    public GameObject rendaSound;
+
+	// Use this for initialization
+	void Start () {
+        buttonText.text = "スターーート！！！";
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isPlaying == true){
+            timer -= Time.deltaTime;
+            timerText.text = timer.ToString("f2");
+        }
+        if (timer < 0){
+            isPlaying = false;
+            timer = 0;
+            timerText.text = timer.ToString("f2");
+            buttonText.text = "終わりーーーーー！！！";
+
+            PlayerPrefs.SetInt("scoore", count);
+            SceneManager.LoadScene("endScene");
+
+        }
+		
+	}
+    public void CountUp(){
+        if (isPlaying == true){
+            count++;
+            countText.text = count.ToString();
+            Debug.Log(count);
+
+            GameObject rendaSoundClone = Instantiate(rendaSound) as GameObject;
+            Destroy(rendaSoundClone, 3.0f);
+        }else{
+            isPlaying = true;
+            buttonText.text = "タップ！";
+            countText.text = "0";
+        }
+    }
+}
